f7294bab7b70f9dc9e88d74e9045703465b6adcc
Jose Eugenio Dos Santos <jose.santos@compasso.com.br>
2023-03-28 12:13:06 -0300

    Initial Libs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+function Home(){
+  const navigation = useNavigation<any>();
+
+  return (
+    <>
+      <Text>Home</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Notifications")}
+      >
+        <Text>Teste</Text>
+      </TouchableOpacity>
+    </>
+  )
+}
+
+function Notifications(){
+  return (
+    <Text>Notifications</Text>
+  )
+}
+
+function Profile(){
+  return (
+    <Text>Profile</Text>
+  )
+}
+
+function Settings(){
+  return (
+    <Text>Settings</Text>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
